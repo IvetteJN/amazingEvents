bfd17d32fbff5abd6646415f11c4c72281526886
Ivette Jael Nobiltá <100625720+IvetteJN@users.noreply.github.com>
2023-03-07 17:14:40 -0300

    corrections

--- details.js
+++ details.js
@@ -1,8 +1,8 @@
 const params = new URLSearchParams(document.location.search)
 const id = params.get("id")
 
-const eventos = data.events
-const evento = eventos.find((evento) => evento.id == id)
+const moreInfo = data.events
+const info = moreInfo.find((info) => info.id == id)
 
 
 const container = document.getElementById("detailContainer")
@@ -11,15 +11,15 @@ let detailCard = ''
 
   detailCard += `
     <div class="card text-center" style="width:18rem;">
-    <img src="${evento.image}" class="card-img-top" alt="...">
+    <img src="${info.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${evento.name}</h5>
-      <p>${evento.date}</p>
-      <p>${evento.category}</p>
-      <p>${evento.capacity}</p>
-      <p>${evento.place}</p>
-      <p class="card-text">${evento.description}</p>
-      <div>Price $ ${evento.price}</div>
+      <h5 class="card-title">${info.name}</h5>
+      <p>${info.date}</p>
+      <p>${info.category}</p>
+      <p>${info.capacity}</p>
+      <p>${info.place}</p>
+      <p class="card-text">${info.description}</p>
+      <div>Price $ ${info.price}</div>
     </div>
   </div>`
 

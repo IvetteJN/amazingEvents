67536a61a4c2213a8ba9f63b5fe1b62aa9f93655
Ivette Jael Nobilta <ivette.nobilta@hotmail.com>
2023-04-14 12:23:07 -0300

    corrections task4

--- scripts/functions.js
+++ scripts/functions.js
@@ -7,9 +7,12 @@ function getEvent(event) {
     <div class="container p-2"><img src=${event.image} class="card-img-top" alt="..."></div>
     <div class="card-body">
       <h5 class="card-title">${event.name}</h5>
-      <p class="card-text">${event.description}</p>
+      <p><strong>Date:</strong> ${event.date}</p>
+      <p><strong>Category:</strong> ${event.category}</p>
+      <p><strong>Capacity:</strong> ${event.capacity}</p>
+      <p><strong>Place:</strong> ${event.place}</p>
+      <p class="card-text"><strong>Description:</strong> ${event.description}</p>
       <div>Price $ ${event.price}</div>
-      <div><a href="../details.html?id=${event.id}" class="btn btn-outline-dark mt-2">+ INFO</a></div>
     </div>
   </div>`
 }
